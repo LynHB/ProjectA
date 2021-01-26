afa8eca56f3f48352db8d3fe4d71cee297c33195
HuangBing <199516@nd.com>
2021-01-26 10:11:48 +0800

    每日更新leecode

--- src/main/java/study/HelloWorld.java
+++ src/main/java/study/HelloWorld.java
@@ -2,54 +2,38 @@ package study;
 
 import org.apache.commons.lang3.time.DateUtils;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.*;
 
 public class HelloWorld {
+    public static void main(String[] args){
+        LocalDate local = LocalDate.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        System.out.println(local.format(dateTimeFormatter));
 
-    int a = 0;
-    boolean flag = false;
+        double d = 0.1415926;
+        DecimalFormat df = new DecimalFormat("#.#####");
+        Double get_double = Double.parseDouble(df.format(d));
+        System.out.println(get_double);
 
-    public void write(){
-        a = 2;
-        flag = true;
-    }
+        LocalDate localDate = LocalDate.now();
+        long timestamp = LocalDate.now().atStartOfDay(ZoneOffset.ofHours(8)).toInstant().toEpochMilli();
 
-    public int read(){
-        int res = -1;
-        if(flag){
-            res = a*a;
-        }
-        return res;
-    }
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        DateTimeFormatter dateTimeHourFormatter = DateTimeFormatter.ofPattern("yyyyMMddHH");
+        System.out.println(localDateTime.format(dateFormatter));
+        System.out.println(localDateTime.format(dateTimeHourFormatter));
 
-    public static void main(String[] args) {
-        ConcurrentHashMap map = new ConcurrentHashMap<Integer, Integer>();
-        for(int i =0;i<1000000;i++){
-            HelloWorld helloWorld = new HelloWorld();
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    helloWorld.write();
-                }
-            });
-            Thread thread2 = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    if(helloWorld.read()==0){
-                        System.out.println("0");
-                    }
 
-                }
-            });
-            thread.start();
-            thread2.start();
-        }
-
-        return;
 
     }
 }
